239ce283a9836abf4586bb9f5bf02df6d9dcc68e
Igor Carvalho <igorweb08@gmail.com>
2015-10-27 11:40:42 -0300

    segurancacspring

---
+++ dump.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.45 : Database - wehavescience
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`wehavescience` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `wehavescience`;
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `role_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `role_description` (`role_description`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`role_id`,`role_description`) values (1,'ROLE_ADMIN'),(2,'ROLE_USER');
+
+/*Table structure for table `user_roles` */
+
+DROP TABLE IF EXISTS `user_roles`;
+
+CREATE TABLE `user_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  KEY `FK73429949902F2193` (`role_id`),
+  KEY `FK734299493559E573` (`user_id`),
+  CONSTRAINT `FK734299493559E573` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FK73429949902F2193` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_roles` */
+
+insert  into `user_roles`(`user_id`,`role_id`) values (1,1),(2,2);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_password` varchar(255) DEFAULT NULL,
+  `user_username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_username` (`user_username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`user_id`,`user_password`,`user_username`) values (1,'admin','admin'),(2,'user','user');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
